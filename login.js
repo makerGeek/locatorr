1ee4a1341c8a1a23654509a9f71154f1bcf1f0e1
makerGeek <moetez.93@gmail.com>
2016-12-13 16:07:00 +0100

    enabled cors

--- login.js
+++ login.js
@@ -1,8 +1,12 @@
+
+//import jQuery
 var script = document.createElement('script');
-script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
+script.src = 'https://code.jquery.com/jquery-1.11.0.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
+
+//post login
 $.ajax({
     type: 'POST',
     // make sure you respect the same origin policy with this url:
@@ -15,4 +19,11 @@ $.ajax({
     success: function(msg){
         alert('wow' + msg);
     }
+});
+
+
+//get groups
+$.get( "http://127.0.0.1:8000/index/api/welcome", function( data ) {
+  console.log(data);
+  alert( "Load was performed." );
 });
